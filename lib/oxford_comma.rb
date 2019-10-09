951228722aa99f7b1c7965882ba11963f035f334
Patrick Ward <patrick.r.ward@verizon.net>
2019-10-09 13:25:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2
     return array.join(" and ")
   elsif array.length >= 3
-    new_last_array_item = "and #{array[-1]}"
-    array.pop
-    array.push(new_last_array_item)
+    array[-1] = "and #{array[-1]}"
     return array.join(", ")
   end
 end
